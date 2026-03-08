4e3d34cd3494c61759840b3add632f8344053fe3
Oliver Wiedner <oliver@Mac-2.lan>
2026-03-07 23:26:34 -0800

    Fix avatar rendering

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "ilyhkkukapwbfhjzhyrq.supabase.co",
-      },
-    ],
+    domains: ["ilyhkkukapwbfhjzhyrq.supabase.co"],
   },
 };
 

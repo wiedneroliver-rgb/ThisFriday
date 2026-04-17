58906efdbeca1a2c43ad367e3521c30728395cd3
jonahmiller2005 <jonah.miller@brentwood.ca>
2026-04-17 15:20:13 -0700

    Add apple-app-site-association for Universal Links

---
+++ This Friday/This Friday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  This Friday
+//
+//  Created by Jonah Miller on 2026-03-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ This Friday/This Friday/This_FridayApp.swift
@@ -0,0 +1,17 @@
+//
+//  This_FridayApp.swift
+//  This Friday
+//
+//  Created by Jonah Miller on 2026-03-08.
+//
+
+import SwiftUI
+
+@main
+struct This_FridayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
